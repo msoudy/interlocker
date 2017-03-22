9caefe64f6649269f32dca8f3c1d6f8e4cdb384b
Jiongjian Chen <jionc@seas.upenn.edu>
2017-03-21 23:49:45 -0400

    Update PartsGraph.h

--- Interlocker/PartsGraph.h
+++ Interlocker/PartsGraph.h
@@ -15,21 +15,41 @@ using namespace std;
 #define FINISHED -1
 #define NOCYCLE -2
 
+
+// axis direction
+#define NO_AXIS 0
+#define X 0
+#define Y 1
+#define Z 2
+
+
 // Class for an undirected graph
-class PartsGraph
+class Graph
 {
 private:
-    int V;    // No. of vertices
-    int index;
-    list<int> *adj;    // Pointer to an array containing adjacency lists
+    int vertexCount;    // No. of vertices
+    
+    // show connection status
+    bool ** connection;
+    
+    // show axis direction of the connection status
+    //" we denote D(A, B) as the set of axial directions that part A can have at its joint with part B."
+    bool *** connection_axis;  // Pointer to an array containing adjacency lists
+    
+    // vector stores cycles of a furniture parts-graph
     std::vector<std::vector<int>> cycles;
     
 public:
-	PartsGraph(int V);   // Constructor
-    ~PartsGraph();       // Destructor
-    void addEdge(int v, int w);   // to add an edge to graph
+    Graph(int V);   // Constructor
+    ~Graph();       // Destructor
+    void addEdge(int v, int w, int direction);   // to add an edge to graph
+    void removeEdge(int v, int w); // to remove an edge of graph
     void setLIGs(std::vector<int>);
     void printLIGs();
-//    void isCycle();
-//    void isCyclicUtil(int v,bool visited[],int parent);
+    
+    // Helper function
+    int helper_union(int index);
+    
+    // Assembly process
+    void constructG1();
 };
