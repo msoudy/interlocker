56975fd8af0d8a80a539e3ff5158b36d33a39399
Mohamed Soudy <msoudy7@gmail.com>
2017-05-08 02:27:34 -0400

    Fix GUI

--- Interlocker/InterlockerCmd.cpp
+++ Interlocker/InterlockerCmd.cpp
@@ -65,20 +65,24 @@ public:
 		MString commands;
 
 		if (nameVal == "Console Table") {
-			string part1 = "polyCube -width 0.2 -height 4.0 -depth 1.5 -n part1; rotate 0 0 90deg part1; move 0 1.5 0 part1;";
-			string part2 = "polyCube -width 0.2 -height 3.0 -depth 1.0 -n part2; move 1.5 0 0 part2;";
-			string part3 = "polyCube -width 0.2 -height 3.2 -depth 1.0 -n part3; rotate 0 0 90deg part3; move 0.2 0.5 -0.2 part3;";
-			string part4 = "polyCube -width 0.2 -height 3.5 -depth 1.1 -n part4; rotate 0 0 90deg part4; move 0.2 -0.8 -0.2 part4;";
-			string part5 = "polyCube -width 0.2 -height 3.0 -depth 1.0 -n part5; rotate 0 90deg 0 part5; move -1.5 0 0 part5;";
-			string part6 = "polyCube -width 0.2 -height 0.7 -depth 0.8 -n part6; rotate 0 0 0 part6; move -1 -1.15 -0.15 part6;";
+			//string part1 = "polyCube -width 0.2 -height 4.0 -depth 1.5 -n part1; rotate 0 0 90deg part1; move 0 1.5 0 part1;";
+			//string part2 = "polyCube -width 0.2 -height 3.0 -depth 1.0 -n part2; move 1.5 0 0 part2;";
+			//string part3 = "polyCube -width 0.2 -height 3.2 -depth 1.0 -n part3; rotate 0 0 90deg part3; move 0.2 0.5 -0.2 part3;";
+			//string part4 = "polyCube -width 0.2 -height 3.5 -depth 1.1 -n part4; rotate 0 0 90deg part4; move 0.2 -0.8 -0.2 part4;";
+			//string part5 = "polyCube -width 0.2 -height 3.0 -depth 1.0 -n part5; rotate 0 90deg 0 part5; move -1.5 0 0 part5;";
+			//string part6 = "polyCube -width 0.2 -height 0.7 -depth 0.8 -n part6; rotate 0 0 0 part6; move -1 -1.15 -0.15 part6;";
 
-			commands += part1.c_str();
-			commands += part2.c_str();
-			commands += part3.c_str();
-			commands += part4.c_str();
-			commands += part5.c_str();
-			commands += part6.c_str();
+			//commands += part1.c_str();
+			//commands += part2.c_str();
+			//commands += part3.c_str();
+			//commands += part4.c_str();
+			//commands += part5.c_str();
+			//commands += part6.c_str();
+
+			string consoletable = "createNode transform -n Interlocker1; createNode mesh - n InterlockerShape1 - p Interlocker1;sets - add initialShadingGroup InterlockerShape1;createNode InterlockerNode - n InterlockerNode1;connectAttr time1.outTime InterlockerNode1.time;connectAttr InterlockerNode1.outputMesh InterlockerShape1.inMesh; ";
+			commands += consoletable.c_str();
 		}
+
 
 		MStatus status = MGlobal::executeCommand(commands);
 		const MStatus failed = MS::kFailure;
